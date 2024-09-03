e58d9aa59351631d90527dbb61ed8e02b3dcd483
Jannatul711 <jfmim711@gmail.com>
2024-09-03 14:25:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function myFunction(){
+    document.getElementById("demo").innerHTML="Are you okay?"
+    document.getElementById("demo").style.backgroundColor="pink" 
+}
+  
+function light(){
+    document.getElementById('myImage').src="images/light-bulb-on.avif"
+    document.getElementById('myImage').src="images/bubl-offpng.png"
+}
+
+function fontSize(){
+    document.getElementById("font").style.fontSize="35px"   
+}
+
+function hidden(){
+    document.getElementById("hide").style.display="none"
+}
+
+function shown(){
+    document.getElementById('hiddenElement').style.display='block'
+}
